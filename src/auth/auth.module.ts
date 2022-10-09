2e9c44fb18e5879432e5fad9da576fd3de1363e8
riccardotornesello <riccardo.tornesello@gmail.com>
2022-10-09 16:06:26 +0000

    fix: correct strategy in auth module

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -4,12 +4,13 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { LocalStrategy } from './strategies/local.strategy';
+import { TokenStrategy } from './strategies/token.strategy';
 import { User } from './entities/user.entity';
 import { Token } from './entities/token.entity';
 
 @Module({
   imports: [TypeOrmModule.forFeature([User, Token]), PassportModule],
-  providers: [AuthService],
-  controllers: [AuthController, LocalStrategy],
+  providers: [AuthService, LocalStrategy, TokenStrategy],
+  controllers: [AuthController],
 })
 export class AuthModule {}
