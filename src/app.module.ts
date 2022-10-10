b1e36c8b428b3bdef2aa7a2042d46aedf64f12ee
riccardotornesello <riccardo.tornesello@gmail.com>
2022-10-10 17:51:49 +0000

    remove typeorm synchronize

--- src/app.module.ts
+++ src/app.module.ts
@@ -15,7 +15,6 @@ import { AuthModule } from './auth/auth.module';
       password: 'root',
       database: 'test',
       autoLoadEntities: true,
-      synchronize: true,
     }),
   ],
   controllers: [AppController],
